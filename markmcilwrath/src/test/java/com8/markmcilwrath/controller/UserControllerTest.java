c909d89311e2bcef098018406372535b73b606ac
markmcilwrath <mcilwrath-m@ulster.ac.uk>
2021-05-12 20:56:15 +0100

    12/05/21

---
+++ markmcilwrath/src/test/java/com8/markmcilwrath/controller/UserControllerTest.java
@@ -0,0 +1,69 @@
+package com8.markmcilwrath.controller;
+
+import com8.markmcilwrath.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Rule;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Collections;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class UserControllerTest {
+
+    private MockMvc mvc;
+
+    @Mock
+    private UserService userService;
+
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets/users");
+
+    private static final String BASE_URL = "/user";
+
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        UserController userController = new UserController(userService);
+        mvc = MockMvcBuilders
+                .standaloneSetup(userController)
+                .apply(documentationConfiguration(this.restDocumentation))
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .alwaysDo(document(
+                        "{method-name}/{step}/",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())))
+                .build();
+
+    }
+
+    @Test
+    public void getUsersTest() throws Exception {
+        when(userService.getAllUsers()).thenReturn(Collections.emptySet());
+
+        mvc.perform(
+                get(BASE_URL)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+
+    }
+
+
+}
